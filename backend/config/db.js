99f6533d36e6649a2c1eec781d8d70376726b839
apattiwar <apattiwar@cdac.in>
2025-04-21 07:15:25 +0530

    use proper env in db

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,8 +2,8 @@ const { Sequelize, DataTypes } = require("sequelize");
 const dotenv = require ('dotenv');
 dotenv.config();
 
-const sequelize = new Sequelize("company", "root", "root", {
-  host: "localhost",  //process.env.DB_HOST
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
+  host: process.env.DB_HOST,
   dialect: "mysql",
 });
 
