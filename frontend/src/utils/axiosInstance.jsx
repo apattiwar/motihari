9585afaf19c3d1cc0ffed1ff51c976356a715853
apattiwar <apattiwar@cdac.in>
2025-04-21 07:44:54 +0530

    add dynamic render backend host address in axiosIZnstance

--- frontend/src/utils/axiosInstance.jsx
+++ frontend/src/utils/axiosInstance.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:8085/api',
+  baseURL: 'https://motihari-1.onrender.com/api',
 });
 
 axiosInstance.interceptors.request.use((config) => {
