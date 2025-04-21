ad09a55b09eec6d7f80e9a8aa1d472f3727fcdca
apattiwar <apattiwar@cdac.in>
2025-04-21 07:30:03 +0530

    add dynamic port inn db

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,11 +1,17 @@
 const { Sequelize, DataTypes } = require("sequelize");
-const dotenv = require ('dotenv');
+const dotenv = require('dotenv');
 dotenv.config();
 
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
   host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
   dialect: "mysql",
+  dialectOptions: {
+    ssl: {
+      rejectUnauthorized: true
+    }
+  },
+  logging: false,
 });
-
 
 module.exports = sequelize;
